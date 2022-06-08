0ca2fc4878b9267e91e180c731188a6f25ffa4a0
Jeddi212 <jedediahf51@gmail.com>
2022-06-08 23:21:45 +0700

    extract snip_snap method

--- src/snip_snap/mod.rs
+++ src/snip_snap/mod.rs
@@ -6,7 +6,11 @@ pub fn run() {
 
     let input = str_to_i32(read_new());
 
-    for i in 1..=input {
+    snip_snap(input);
+}
+
+fn snip_snap(n: i32) {
+    for i in 1..=n {
         match (i%3, i%5) {
             (0, 0) => print!("snip-snap"),
             (0, _) => print!("snip"),
